90910bd7f92e61dc3dc721412be47da88f4c4db4
Michael Jardine <michaeljardine92@gmail.com>
2024-05-29 22:49:54 +0100

    adding R identified variables

--- refined models.py
+++ refined models.py
@@ -22,6 +22,7 @@ from sklearn.model_selection import train_test_split
 #%%
 
 ## read in the data
+
 ## two sets:  trainign and test data
 
 # training data
@@ -49,10 +50,37 @@ y = home_data.SalePrice
 
 #%%
 
-## now select some features to train the model
-features = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
+## choosing features from analysis done in R
+features_R = ["LotFrontage", "OverallQual", "YearRemodAdd", "MasVnrArea", "GrLivArea", "FullBath", "TotRmsAbvGrd", 
+            "Fireplaces", "GarageCars", "GarageArea", "WoodDeckSF"  ]
 
 ## now select the columsn from the training data that fit those features
 # call this X
-X = home_data[features]
+X = home_data[features_R]
+
+
+#%%
+
+## now one way to do this is to split the home_data into a traing and test set
+## not really needed here but done for properity at this time
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+#%%
+
+## now build a random forest model using these four sets 
+rf_model_R = RandomForestRegressor(random_state=1)
+rf_model_R.fit(train_X, train_y)
+rf_val_pred_R = rf_model_R.predict(val_X)
+rf_val_mae_R = mean_absolute_error(rf_val_pred_R, val_y)
+
+## error with NAn values
+## so do we just remove them or haow can be get rnadom forest to deal with them?
+
+#%%
+
+## ok so now we find what the erros in the orediction actually are
+print("Validation MAE for Random Forest Model {:,.0f}".format(rf_val_mae_R))
+
+
+
 
